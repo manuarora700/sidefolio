e87c4f3374611e5fe358d800d2cd1a91229f78c6
Manu Arora <manuarorawork@gmail.com>
2023-08-24 11:56:07 +0530

    add: about us fixes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -17,7 +17,37 @@ export default function About() {
     <Container>
       <span className="text-4xl">💬</span>
       <Heading className="font-black">About Me</Heading>
-      <div className="flex my-10">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-10 my-10">
+        <Image
+          src="https://images.unsplash.com/photo-1692544350322-ac70cfd63614?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60"
+          width={200}
+          height={400}
+          alt="about"
+          className="rounded-md object-cover transform rotate-3 shadow-xl block w-full h-40 md:h-60 hover:rotate-0 transition duration-200"
+        />
+        <Image
+          src="https://images.unsplash.com/photo-1692374227159-2d3592f274c9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60"
+          width={200}
+          height={400}
+          alt="about"
+          className="rounded-md object-cover transform -rotate-3 shadow-xl block w-full h-40 md:h-60  hover:rotate-0 transition duration-200"
+        />
+        <Image
+          src="https://images.unsplash.com/photo-1692005561659-cdba32d1e4a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+          width={200}
+          height={400}
+          alt="about"
+          className="rounded-md object-cover transform rotate-3 shadow-xl block w-full h-40 md:h-60  hover:rotate-0 transition duration-200"
+        />
+        <Image
+          src="https://images.unsplash.com/photo-1692445381633-7999ebc03730?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzM3x8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+          width={200}
+          height={400}
+          alt="about"
+          className="rounded-md object-cover transform -rotate-3 shadow-xl block w-full h-40 md:h-60  hover:rotate-0 transition duration-200"
+        />
+      </div>
+      {/* <div className="flex my-10">
         <Image
           src="/images/about.webp"
           width={800}
@@ -25,7 +55,7 @@ export default function About() {
           alt="about"
           className="rounded-md"
         />
-      </div>
+      </div> */}
       <div className="max-w-4xl">
         <Paragraph className=" mt-4">
           Hey there, I&apos;m John Doe - a passionate developer, avid writer,
