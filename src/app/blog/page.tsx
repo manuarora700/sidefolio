6b7e32bd997ada08d00d85abeb6a664d6f6184bd
Manu Arora <manuarorawork@gmail.com>
2023-08-28 12:38:00 +0530

    add: blogs metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,6 +5,13 @@ import { Paragraph } from "@/components/Paragraph";
 import { Products } from "@/components/Products";
 import { getAllBlogs } from "../../../lib/getAllBlogs";
 import { Blogs } from "@/components/Blogs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Blogs | John Doe",
+  description:
+    "John Doe is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
+};
 
 export default async function Blog() {
   const blogs = await getAllBlogs();
